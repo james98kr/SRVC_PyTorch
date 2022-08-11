9dc1e9bfea46e930d237594a4ddbc2faaf09223f
James <james98kr@gmail.com>
2022-08-11 14:30:59 +0900

    Erased comments

--- utils/video_reader.py
+++ utils/video_reader.py
@@ -30,32 +30,6 @@ def get_segment_frames(cap, frame_per_segment):
         return frames
 
 
-# def YUV2RGB(yuv):
-#     m = np.array([[1.0, 1.0, 1.0],
-#                 [-0.000007154783816076815, -0.3441331386566162, 1.7720025777816772],
-#                 [1.4019975662231445, -0.7141380310058594, 0.00001542569043522235]])
-#     h = int(yuv.shape[0] / 1.5)
-#     w = yuv.shape[1]
-#     y = yuv[:h]
-#     h_u = h // 4
-#     h_v = h // 4
-#     u = yuv[h:h + h_u]
-#     v = yuv[-h_v:]
-#     u = np.reshape(u, (h_u * 2, w // 2))
-#     v = np.reshape(v, (h_v * 2, w // 2))
-#     u = cv2.resize(u, (w, h), interpolation=cv2.INTER_CUBIC)
-#     v = cv2.resize(v, (w, h), interpolation=cv2.INTER_CUBIC)
-#     yuv = np.concatenate([y[..., None], u[..., None], v[..., None]], axis=-1)
-
-#     bgr = np.dot(yuv, m)
-#     bgr[:, :, 0] -= 179.45477266423404
-#     bgr[:, :, 1] += 135.45870971679688
-#     bgr[:, :, 2] -= 226.8183044444304
-#     bgr = np.clip(bgr, 0, 255)
-
-#     return bgr.astype(np.uint8)
-
-
 class VideoCaptureYUV:
     def __init__(self, filename, size):
         self.width, self.height = size
